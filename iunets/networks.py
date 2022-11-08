6b5f1493f0cf3403cec0da460155e0bdefbfdecd
Lukas Herron <lukasherron@core.mynetworksettings.com>
2022-11-07 20:49:26 -0500

    fixed sinusoidal pos emb device

--- iunets/networks.py
+++ iunets/networks.py
@@ -743,10 +743,10 @@ class SinusoidalPosEmb(nn.Module):
         self.dim = dim
 
     def forward(self, x):
-        # device = x.device
+        device = x.device
         half_dim = self.dim // 2
         emb = math.log(10000) / (half_dim - 1)
-        emb = torch.exp(torch.arange(half_dim) * -emb)
+        emb = torch.exp(torch.arange(half_dim) * -emb).to(device)
         emb = x[:, None] * emb[None, :]
         emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
         return emb
